6a0f4fbb5854ce236a73a411e054f18644dd4008
OkezieE <okezie@gmail.com>
2014-02-20 07:06:52 -0800

    Small bug with forcing exits

--- install/mac/install.sh
+++ install/mac/install.sh
@@ -15,7 +15,7 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-[[ -z $1 ]] && echo "Enter your account ID as an argument. e.g. install.sh 2342342sdfwe23423423243423" ; exit 1
+[[ -z $1 ]] && exit "Enter your account ID as an argument. e.g. install.sh 2342342sdfwe23423423243423"
 
 LE_LOCATION="https://raw.github.com/logentries/le/master/le"
 
